5199a924df06830028dd8562fb1246ac3c891ea9
Victor Santos <victor.ssouza@hotmail.com.br>
2016-08-22 14:47:40 -0300

    Remove 'junk' directory && Add some unit_tests for dialog box.

--- Junk/README.txt
+++
@@ -1,6 +0,0 @@
-Use your virtual environment to test the app!
-
-Basic instructions:
-
-1) Install python’s virtual env:
-	pip install virtualenv
--- unit_tests/simple_front.py
+++ unit_tests/simple_front.py
@@ -1,14 +1,26 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# -'''- coding: utf-8 -'''-
 
+#import __future__
 from dialog import Dialog
+import sys, re
+import subprocess
+
+reload(sys)
+sys.setdefaultencoding('utf-8')
 
 d = Dialog()
+
+non_utf_character='Counter-Strike without utf-8: Céaser Útopic'
+formatted = subprocess.call(non_utf_character.encode(sys.getfilesystemencoding()))
+
 
 
 code, value = d.radiolist('Please, choose one game for install', width=40, height=10, list_height=3, choices=[\
 ('204300', 'Awesomenauts', 0),\
 ('240', 'Counter-Strike: Source', 0),\
+('340', formatted, 0),\
 ('377160', 'Fallout 4', 0)])
 
 if value == '204300':
