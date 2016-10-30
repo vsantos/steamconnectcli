ab3bfe9f5dccc57591ef647f03a6024b6f58feff
Victor Santos <victor.ssouza@hotmail.com.br>
2016-10-30 18:57:24 -0200

    feat(core): port to python 3
    Due an encoding issues we were forced to move from Python 2
      to Python 3, which required a lot of 're-coding'.

--- CLI/scl.py
+++ CLI/scl.py
@@ -6,10 +6,11 @@ import requests
 import time
 import base64
 import json
+import demjson
 import codecs
 import getpass
 import pickle
-import cookielib
+import http.cookiejar
 import os, sys
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_v1_5
@@ -24,11 +25,12 @@ class SteamConnect:
 			os.system("mkdir /tmp/chimera_os/")
 
 	def getting_credentials(self):
-		if os.path.isfile('/tmp/chimera_os/cookies.tmp') == False or os.path.isfile('/tmp/chimera_os/user_info.txt') == False:	
-			self.steam_user = raw_input("Username: ")
-			self.steam_password = getpass.getpass("Password: ")
+		if os.path.isfile('/tmp/chimera_os/cookies.tmp') == False or os.path.isfile('/tmp/chimera_os/user_info.txt') == False:
+			self.steam_user = input("Username: ")
+			steam_temp_password = getpass.getpass("Password: ")
+			self.steam_password = str.encode(steam_temp_password)
 		else:
-			print ("You already have the files")
+			print ("[INFO]: You are already logged.")
 
 	def get_rsa_from_steam(self, steam_user, steam_password):
 		URL_RSA = 'https://steamcommunity.com/login/getrsakey/'
@@ -48,7 +50,7 @@ class SteamConnect:
 			"donotcache": str(int(time.time()*1000)),
 		}
 		values_for_rsa = {
-			'username' : steam_user, 
+			'username' : steam_user,
 			'donotcache' : str(int(time.time()*1000)),
 		}
 
@@ -57,17 +59,15 @@ class SteamConnect:
 			data_rsa = response.json()
 			self.values_for_login.update({'rsatimestamp':data_rsa['timestamp']})
 
-			if data_rsa['success'] == True:
-				print ("Got RSA keys successfully.\n")
+			if data_rsa['success']:
+				print ("[INFO]: Got RSA keys successfully.\n")
 
-			mod = long(str(data_rsa['publickey_mod']), 16)
-			exp = long(str(data_rsa['publickey_exp']), 16)
+			mod = int(str(data_rsa['publickey_mod']), 16)
+			exp = int(str(data_rsa['publickey_exp']), 16)
 			rsa = RSA.construct((mod,exp))
 			cipher = PKCS1_v1_5.new(rsa)
 			self.values_for_login.update({'password': base64.b64encode(cipher.encrypt(steam_password))})
-			print ("Password encrypted. \n")
-			#For debugging purposes
-			#print self.values_for_login
+			print ("[INFO]: Password encrypted. \n")
 
 	def do_steam_login(self, values_for_login):
 		URL_DO_LOGIN = 'https://steamcommunity.com/login/dologin/'
@@ -77,96 +77,110 @@ class SteamConnect:
 				response_for_login = c.post(URL_DO_LOGIN, self.values_for_login)
 				data_login = response_for_login.json()
 			except:
-				print ("Could not login, did you checked your password? \n")
+				print ("[ERROR]: Could not login, did you checked your password? \n")
 
 			if data_login['success'] == False:
 				try:
 					if data_login['message'] == "Please verify your humanity by re-entering the characters below.":
-						print ("Are you human? Please enter captcha or try again later.\n")
+						print ("[ERROR]: Are you human? Please enter captcha or try again later.\n")
 				except:
-					print ("")
+					pass
 
 				try:
 					if data_login['message'] == "There have been too many login failures from your network in a short time period.  Please wait and try again later.":
-						print ("Too many login failures from your network, try again later. \n")
+						print ("[ERROR]: Too many login failures from your network, try again later. \n")
 				except:
-					print ("")
+					pass
 
 				try:
-					if data_login['emailauth_needed'] == True:
-						email_code_auth = raw_input("Please check your e-mail and input Steam Mail code, you need to allow this operation: ")
+					if data_login['emailauth_needed']:
+						email_code_auth = input("[INFO]: Please check your e-mail and input Steam Mail code, you need to allow this operation: ")
 						self.values_for_login.update({'emailauth':email_code_auth})
 						self.do_steam_login(self.values_for_login)
 				except:
-					if data_login['requires_twofactor'] == True:
-						print ("Not able to trigger e-mail auth, trying Steam Guard.")
-						mobile_code_auth = raw_input("Please check your mobile and input Steam Guard Code, you need to allow this operation: ")
+					pass
+
+				try:
+					if data_login['requires_twofactor']:
+						print ("[INFO]: Not able to trigger e-mail auth, trying Steam Guard.")
+						mobile_code_auth = input("[INFO]: Please check your mobile and input Steam Guard Code, you need to allow this operation: ")
 						self.values_for_login.update({'twofactorcode':mobile_code_auth})
 						self.do_steam_login(self.values_for_login)
+				except:
+					pass
 
-			if data_login['success'] == True:
-				print ("User logged in.")
+			if data_login['success']:
+				print ("[INFO]: User logged in.")
 				#print data_login
-				
+
 				with open('/tmp/chimera_os/user_info.txt', 'w') as ui:
 					ui.write(data_login['transfer_parameters']['steamid'])
-					print ("== User's info updated. ==")
+					print ("[INFO]: User's info updated. ")
 
 				with open("/tmp/chimera_os/steam_username.txt","w") as h:
 					h.write(values_for_login['username'])
-					print ("== Username updated. ==")
+					print ("[INFO]: Username updated. ")
 
-				with open('/tmp/chimera_os/cookies.tmp', 'w') as f:
-					pickle.dump(requests.utils.dict_from_cookiejar(response_for_login.cookies), f)
-					print ("== Cookies importados. ==")
+				with open('/tmp/chimera_os/steam_cookies.tmp', 'w') as f:
+
+					jar = http.cookiejar.LWPCookieJar(filename="/tmp/chimera_os/steam.cookies")
+					print (response_for_login.cookies)
+					for c in response_for_login.cookies:
+					    jar.set_cookie(c)
+					jar.save(ignore_discard=True)
+					print (jar)
+					print ("[INFO]: Cookies imported.")
 
 	def getting_user_library(self):
 		if os.path.isfile('/tmp/chimera_os/user_info.txt') == False:
-			print ("You have to log in.")
-			time.sleep(2)
-			sys.exit()
+			print ("[ERROR]: Please, log in before continue.")
+			time.sleep(3)
+			sys.exit(1)
 
 		try:
 			file = open('/tmp/chimera_os/user_info.txt','r')
 			USER_ID64 = file.read()
 			file.close()
 		except:
-			print ("Seens that we do not have a needed file, please check your credentials then try again.")
+			print ("[ERROR]: Seens that we do not have a credential needed file, please check it then try again.")
 
 		URL_USER_PROFILE = 'http://steamcommunity.com/profiles/'
 		URL_USER_ALL_LIBRARY = URL_USER_PROFILE + USER_ID64 +'/games/?tab=all'
-		print ("Connecting to your library...\n")
+		print ("[INFO]: Connecting to your library")
 
 		try:
-			with open('/tmp/chimera_os/cookies.tmp') as f:
-				cookie = requests.utils.cookiejar_from_dict(pickle.load(f))
+			jar2 = http.cookiejar.LWPCookieJar()
+			jar2.load(filename="/tmp/chimera_os/steam.cookies")
 		except:
-			print("I could not open the 'cookies' file properly. Please check if everything is ok.")	
+			print("[ERROR]: I could not open the 'cookies' file properly. Please check if everything is ok.")
 
 		with requests.Session() as c:
 			try:
-				response_for_games = c.get(URL_USER_ALL_LIBRARY, cookies=cookie)
+				response_for_games = c.get(URL_USER_ALL_LIBRARY, cookies=jar2)
+				#print (response_for_games.status_code)
 				data = str(response_for_games.content)
 			except:
-				print ('Unable to access your library, for your safety we are removing the cookie file under your installation directory.')
-			
-			#try:
+				print ('[ERROR]: Unable to access your library, for your safety we are removing the cookie file under your installation directory.')
+
+			#Here we are wrapping the html page for getting only the games json:
 			first_split_left, first_split_right = data.split("rgGames =")
 			first_split_right = first_split_right[1:]
-			second_split_left, second_split_right = first_split_right.split("var rgChangingGames =")
-			second_split_left = second_split_left[:-5]
+			second_split_left, second_split_right = first_split_right.split(" rgChangingGames =")
+			second_split_left = second_split_left[:-12]
 			final_all_games = json.dumps(second_split_left)
 			final_json_all_games = json.loads(final_all_games)
 
-			#Now lets split out on shell the user's library
+			#Now we have to treat the json to our purposes
 			games_count = 0
-			games = []
 			with codecs.open('/tmp/chimera_os/user_library.json', 'w', encoding='utf-8') as ul:
-				print ("Inserting user's library")
+				print ("[INFO]: Inserting user's library...")
 
-				#try:
 				user_games = []
-				for key in json.loads(final_json_all_games):
+
+				# The html 'json' have some escaped characters which is not readble as a real json, so we need to decode
+				decoded = demjson.decode(final_json_all_games)
+
+				for key in decoded:
 					games_count += 1
 
 					gameid = key['appid']
@@ -179,9 +193,9 @@ class SteamConnect:
 					user_games.append(game_list)
 				#except:
 					#print ("Something went wrong when splitting user's library.")
-				#print user_games	
+				#print user_games
 				json.dump(user_games, ul)
-				print ("Your user currently have an amount of "+str(games_count)+" games.\n")
+				print ("[INFO]: Your user currently have an amount of "+str(games_count)+" games.\n")
 
 	def list_friends(self):
 		self.STEAM_API_KEY = '219AE164017EB5546AE1C6059ECDBF8A'
@@ -190,13 +204,13 @@ class SteamConnect:
 			USER_ID64 = file.read()
 			file.close()
 		except:
-			print ("Seens that we do not have a needed file, please check your credentials then try again.")
+			print ("[ERROR]: Seens that we do not have a credential needed file, please check it then try again.")
 
 		MY_FRIENDS_URL = "http://api.steampowered.com/ISteamUser/GetFriendList/v0001/?key=%s&steamid=%s&relationship=friend" %(self.STEAM_API_KEY, USER_ID64)
 		response = requests.get(MY_FRIENDS_URL)
 		jsondata = response.json()
 		if response.status_code == 401:
-			print ("Your profile is private, honey.")
+			print ("[ERROR]: Your profile is private, we can't continue.")
 		else:
 			#Loop for getting friend's info
 			for f in jsondata['friendslist']['friends']:
@@ -216,17 +230,17 @@ class SteamConnect:
 						elif f['personastate'] == 2:
 							print ("Status: Busy")
 						elif f['personastate'] == 3:
-							print ("Status: Away")	
+							print ("Status: Away")
 						elif f['personastate'] == 4:
 							print ("Status: Snooze Zzzzzz....")
 						elif f['personastate'] == 5:
 							print ("Status: Currently in trade or looking for one")
 						elif f['personastate'] == 6:
-							print ("Status: Playing")						
+							print ("Status: Playing")
 						else:
-							print ("Status: Unknown status! Get help.")	
+							print ("Status: Unknown status! Get help.")
 						print ("")
-											
+
 				except:
 					friend_name = f['realname']
 					print ("Nick: %s") %(friend_name.encode('utf-8'))
@@ -237,27 +251,27 @@ class SteamConnect:
 					elif f['personastate'] == 2:
 						print ("Status: Busy")
 					elif f['personastate'] == 3:
-						print ("Status: Away")	
+						print ("Status: Away")
 					elif f['personastate'] == 4:
 						print ("Status: Snooze Zzzzzz....")
 					elif f['personastate'] == 5:
 						print ("Status: Currently in trade")
 					elif f['personastate'] == 6:
-						print ("Status: Playing something")						
+						print ("Status: Playing something")
 					else:
-						print ("Status: Unknown status! Get help.")	
+						print ("Status: Unknown status! Get help.")
 					print ("")
 
 	def remove_local_cookie_files(self):
-		if os.path.isfile('/tmp/chimera_os/cookies.tmp') == False and os.path.isfile('/tmp/chimera_os/user_info.txt') == False and os.path.isfile('steam_username.txt') == False:
-			print ("You do not have any stored files to delete.\n")
-		#if os.path.isfile('cookies.tmp') == True and os.path.isfile('user_info.txt') == True and os.path.isfile('steam_username.txt') == False:
+		if os.path.isfile('/tmp/chimera_os/steam.cookies') == False and os.path.isfile('/tmp/chimera_os/user_info.txt') == False and os.path.isfile('steam_username.txt') == False:
+			print ("[INFO]: You do not have any stored files to delete.\n")
 		else:
 			try:
-				os.remove('/tmp/chimera_os/cookies.tmp')
-				os.remove('/tmp/chimera_os/user_info.txt')
 				os.remove('/tmp/chimera_os/steam_username.txt')
+				os.remove('/tmp/chimera_os/steam.cookies')
+				os.remove('/tmp/chimera_os/user_info.txt')
 				os.remove('/tmp/chimera_os/user_library.json')
+
 			except:
 				print ("File not found.")
-			print ("Files deleted, please create a new session. \n")
+			print ("[INFO]: Files deleted, please create a new session. \n")
--- unit_tests/simple_front.py
+++ unit_tests/simple_front.py
@@ -1,31 +1,47 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# -'''- coding: utf-8 -'''-
-
-#import __future__
+#- *- coding: utf- 8 - *-
+#!/usr/bin/env python3
+import os
+import json
+import codecs
+import operator
 from dialog import Dialog
-import sys, re
-import subprocess
 
-reload(sys)
-sys.setdefaultencoding('utf-8')
+def parsing_user_library_to_json():
+	print ("== Offline logic test for: 'Parsing Steam's library ==")
 
-d = Dialog()
+	if os.path.isfile('mocked/user_library.json') == True:
+		f_output = codecs.open('mocked/user_library.json', 'r', encoding='utf-8')
+		temp_library = json.load(f_output)
 
-non_utf_character='Counter-Strike without utf-8: Céaser Útopic'
-formatted = subprocess.call(non_utf_character.encode(sys.getfilesystemencoding()))
+		user_games_list = []
+		for game in temp_library:
+			user_games_list.append((str(game['gameid']), game['name'], 0))
+		
+		user_games_list.sort(key=operator.itemgetter(1))
+		return True
+	else:
+		return False
 
+def game_choice_test():
+	print ("== Offline logic test for: 'Game choice from Dialog' ==")	
 
+	if os.path.isfile('mocked/user_library_parsed.txt') == True:
+		f = open()
+		temp
+		temp_library = [line.strip() for line in open("mocked/user_library_parsed.txt", 'r')]
 
-code, value = d.radiolist('Please, choose one game for install', width=40, height=10, list_height=3, choices=[\
-('204300', 'Awesomenauts', 0),\
-('240', 'Counter-Strike: Source', 0),\
-('340', formatted, 0),\
-('377160', 'Fallout 4', 0)])
+		d_box = Dialog()
+		code, get_gameid = d_box.radiolist("You currently have " +str(len(temp_library))+ " games. Please, choose one of them to install.", width=200, height=100, list_height=20,choices=temp_library)
 
-if value == '204300':
-	d.msgbox("Awesomenauts escolhido para download.", height=10, width=45)
-elif value == '240':
-	d.msgbox("Counter-Strike Source escolhido para download.", height=10, width=45)
-elif value == '377160':
-	d.msgbox("Fallout 4 escolhido para download. ;D", height=10, width=45)
+		return True
+	else:
+		return False
+
+if parsing_user_library_to_json() == True:
+	print ("Passed.")
+else:
+	print ("Failed.")
+if game_choice_test() == True:
+	print ("Passed.")
+else:
+	print ("Failed.")
